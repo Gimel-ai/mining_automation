46ea84d289c3dec6cf0974660b33003b3889085e
Ruben Roberto <61117131+Gimel-ai@users.noreply.github.com>
2021-01-13 14:20:43 -0500

    update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo git clone https://github.com/Gimel-ai/mining_automation.git
+timeout 180 sudo docker pull gimel12/cuda_deth:v1.1
 
-cd /home/bizon/mining_automation
+timeout 10800 sudo docker run --gpus all -v ~/docker_workspace:/workspace/work -it --shm-size=1g --ulimit memlock=-1  --ulimit stack=67108864 --rm 6e0214be53f0
 
-sudo ./deploy.sh
